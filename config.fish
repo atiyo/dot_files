183239f31baac59b025debe45d18e5a024be574e
Atiyo Ghosh <atiyo.ghosh@creditkudos.com>
2020-01-20 16:54:59 +0000

    Fish vi keybindings

--- config.fish
+++ config.fish
@@ -3,3 +3,4 @@ alias fishconf "vim ~/.config/fish/config.fish"
 alias vimconf "vim ~/.config/nvim/init.vim"
 alias cds "cd (fd -t f | fzf | xargs dirname)"
 alias his "eval (history | fzf)"
+fish_vi_key_bindings
