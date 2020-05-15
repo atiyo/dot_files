30717fa630746888c715009355190da522b64aa5
atiyo <atiyo@users.noreply.github.com>
2020-05-15 15:11:57 +0100

    look_in look_for fish helpers

--- config.fish
+++ config.fish
@@ -3,4 +3,10 @@ alias fishconf "vim ~/.config/fish/config.fish"
 alias vimconf "vim ~/.config/nvim/init.vim"
 alias cds "cd (fd -t f | fzf | xargs dirname)"
 alias his "eval (history | fzf)"
+function look_in
+    fd $argv[1] | xargs rg $argv[2]
+end
+function look_for
+    fd $argv[1]
+end
 fish_vi_key_bindings
