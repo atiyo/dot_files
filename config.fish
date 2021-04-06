bc921de915df22ea971b2242945357358adf7575
Atiyo Ghosh <atiyoghosh@Atiyos-Air.lan>
2021-04-06 09:44:11 +0100

    add git aliases

--- config.fish
+++ config.fish
@@ -3,6 +3,13 @@ alias fishconf "vim ~/.config/fish/config.fish"
 alias vimconf "vim ~/.config/nvim/init.vim"
 alias cds "cd (fd -t f | fzf | xargs dirname)"
 alias his "eval (history | fzf)"
+alias gad "git add"
+alias gch "git checkout"
+alias gco "git commit"
+alias gdiff "git diff"
+alias gpul "git pull"
+alias gpus "git push"
+alias gst "git status"
 function li
     fd $argv[1]\$ | xargs rg $argv[2]
 end
