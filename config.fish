192a7f8de919271317834454ba1269664b9be6db
Atiyo Ghosh <atiyo@users.noreply.github.com>
2022-08-18 10:54:27 +0100

    Add hydro colours

--- config.fish
+++ config.fish
@@ -10,14 +10,24 @@ alias gdiff "git diff"
 alias gpul "git pull"
 alias gpus "git push"
 alias gst "git status"
+
 function li
     fd $argv[1]\$ | xargs rg $argv[2]
 end
+
 function lf
     fd $argv[1]
 end
+
 function save
    git add $argv[1..-2]
    git commit -m $argv[-1] 
 end
+
+set -g hydro_color_pwd B8BB26
+set -g hydro_color_git 83A598
+set -g hydro_color_error FB4934
+set -g hydro_color_prompt 928374
+set -g hydro_color_duration FABD2F
+
 fish_vi_key_bindings
