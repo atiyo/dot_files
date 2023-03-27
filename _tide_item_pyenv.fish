d557a9abbffcb003f9cd240ff242d3ed5d9c755f
Atiyo Ghosh <atiyo@users.noreply.github.com>
2023-03-27 11:45:10 +0100

    add comments on tide pyenv prompt creation

--- _tide_item_pyenv.fish
+++ _tide_item_pyenv.fish
@@ -1,3 +1,5 @@
+# to enable: set --apend tide_right_prompt_items pyenv
+# to disable: set -e tide_right_prompt_items[-1]
 function _tide_item_pyenv
     if set -q PYENV_VERSION
         _tide_print_item pyenv $tide_virtual_env_icon' ' $PYENV_VERSION
